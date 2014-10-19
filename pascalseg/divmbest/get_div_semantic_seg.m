beaf06c06947bd831f50c83f976d3c606037bbcf
senthilps8 <senthilps8@gmail.com>
2014-10-19 17:22:47 -0400

    changing wget and tar commands

--- pascalseg/divmbest/get_div_semantic_seg.m
+++ pascalseg/divmbest/get_div_semantic_seg.m
@@ -164,8 +164,8 @@ function segs = get_div_semantic_seg(img, masks, lambda, num_solutions)
 function img_names=prepare_data(exp_dir, img, masks, mask_type,img_set)
 
 	if(~exist(exp_dir,'dir'))
-		!wget https://filebox.ece.vt.edu/~senthil/divseg_o2p_env.tar.gz
-		!tar xfz divseg_o2p_env.tar.gz
+		system('wget https://filebox.ece.vt.edu/~senthil/divseg_o2p_env.tar.gz');
+		system('tar -xfz divseg_o2p_env.tar.gz');
 	end
 
 	if(isstr(img))
